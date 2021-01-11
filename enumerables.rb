4e2886e884ad281a313aa302485ec490ee87cca5
Prerna <ppriyali0711@gmail.com>
2021-01-11 18:27:58 +0530

    changes

--- enumerables.rb
+++ enumerables.rb
@@ -171,4 +171,4 @@ end
 # rubocop:enable Metrics/CyclomaticComplexity
 # rubocop:enable Metrics/PerceivedComplexity
 # rubocop:enable Metrics/MethodLength
-# rubocop:enable Metrics/ModuleLength
+# rubocop:enable Metrices/ModuleLength
